661848d8fb8bd0ddd06ace5ea13efd855c9d9ac9
Philipp Bredol <philipp.bredol@rwth-aachen.de>
2024-10-11 15:14:40 +0200

    increment version number

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 from setuptools.command.install import install
 
 # DO NOT CHANGE: this line will be replaced by ./dev/update-setup.py
-version = '1.0.7+h37eb092'
+version = '1.0.8'
 
 # if setup is run in project dir, update version number
 try:
